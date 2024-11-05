a9492fd0ecfb19a47d34b682cf1fbba508d2f7f0
Sakshi Kumari <sakshikumare2004@gmail.com>
2024-11-05 22:27:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import PostCards from './Component/Card.jsx';
+import './Component/Card.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      <PostCards />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Card.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const PostCards = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  return (
+    <div className="container">
+      {posts.map(post => (
+        <div key={post.id} className="card">
+          <div className="card-body">
+            <h5 className="card-title">{post.title}</h5>
+            <p className="card-text">{post.body}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostCards;
